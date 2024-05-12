863a59600fd81083161bc49b0df8b5a3cad90040
jimmiejams <7391874+jimmiejams@users.noreply.github.com>
2024-05-12 16:04:25 +1000

    The benchmark world got too difficult to maintain, with the addition of the time parameters, so just use random_world() to do benchmarking now.  Statistically the distribution will be the same.

--- benches/render.rs
+++ benches/render.rs
@@ -1,14 +1,15 @@
 use std::time::Duration;
 use criterion::{criterion_group, criterion_main, Criterion};
 use indicatif::ProgressBar;
-use raytracer::benchmark_world::benchmark_world;
+
 use raytracer::camera::Camera;
+use raytracer::random_world::random_world;
 use raytracer::raytracer::{OutputImageParams, Raytracer};
 use raytracer::vec3::Vec3;
 
 fn render() {
     let output_image_params = OutputImageParams::new(3.0 / 2.0, 400);
-    let world = benchmark_world();
+    let world = random_world();
     let camera = Camera::new(
         Vec3::new(13.0, 2.0, 3.0),
         Vec3::new(0.0, 0.0, 0.0),
